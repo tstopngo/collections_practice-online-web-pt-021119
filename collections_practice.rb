f65d7413dc1afbf569a4b6f23b5b077d5d3ba5ad
Tracy Ngo <tngo1206@gmail.com>
2019-02-20 06:24:50 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,61 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort { |x,y| y <=> x }
+end
+
+def sort_array_char_count(array)
+  array.sort  {|x,y| x.length <=> y.length}
+end 
+
+def swap_elements(array)
+  element_2 = array[1]
+  element_3 = array[2]
+  
+  array[1]= element_3
+  array[2] = element_2
+
+  array
+  
+end
+
+def reverse_array(array)
+  array.reverse 
+end
+
+def kesha_maker(array)
+  array.map do |string|
+    string[2] ='$'
+  end
+  array
+end
+
+def find_a(array)
+  a_array = []
+  array.each do |word|
+   if  word.start_with?('a')
+     a_array << word
+    end
+  end
+  a_array
+  end
+  
+  def sum_array(array)
+    sum = 0
+    array.each do |number|
+      sum = sum + number
+    end
+    sum
+  end
+  
+  def add_s(array)
+    array.map do |word|
+     if word != array[1]
+       word + 's'
+      else
+       word
+     end
+    end
+  end
